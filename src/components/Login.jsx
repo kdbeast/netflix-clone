2ec6bfd4cdea0e76b958201efd88ef5b7b34f477
Karan <karanjamwal2004@gmail.com>
2025-07-02 20:28:12 +0530

    signin/signup-form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
       <div className="absolute">
         <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_small.jpg"
-          alt=""
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_medium.jpg"
+          alt="logo"
         />
       </div>
-      <form className=" relative p-12 w-3/12 bg-black">
+      <form className=" w-3/12 mx-auto absolute p-12 bg-black/70 my-36 right-0 left-0 text-white rounded-lg">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && <input
+          type="text"
+          placeholder="Full Name"
+          className="p-3 my-3 w-full bg-gray-700"
+        />}
         <input
           type="text"
-          placeholder="Email or phone number"
-          className="p-2 m-2"
+          placeholder="Email Address"
+          className="p-3 my-3 w-full bg-gray-700"
         />
-        <input type="text" placeholder="Password" className="p-2 m-2" />
-        <button className="p-4 m-4">Sign In</button>
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 my-3 w-full bg-gray-700"
+        />
+        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 text-sm cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now."
+            : "Already registered? Sign In Now."}
+        </p>
       </form>
     </div>
   );
